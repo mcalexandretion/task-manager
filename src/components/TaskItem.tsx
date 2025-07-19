bf09e34f6d1edfc1251c03e538e0f10268f708e9
Alexandra <sashaf708@gmail.com>
2025-07-19 20:41:43 +0400

    feat: add TaskItem

---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,58 @@
+import { useNavigate } from "react-router-dom";
+import { Card, CardContent, Typography, Chip, Button, Box} from '@mui/material';
+import type { Task } from '../types/task';
+import { TaskCategory, TaskPriority, TaskStatus } from '../types/task';
+import styles from '../styles/TaskItem.module.css';
+
+interface TaskItemProps {
+    task: Task;
+}
+
+const getPriorityColor = (priority: TaskPriority) => {
+    switch(priority) {
+        case TaskPriority.High: return 'error';
+        case TaskPriority.Medium: return 'warning';
+        case TaskPriority.Low: return 'success';
+        default: return 'default';
+    }
+};
+
+const getStatusColor = (status: TaskStatus) => {
+    switch (status) {
+        case TaskStatus.Done: return 'success';
+        case TaskStatus.InProgress: return 'warning';
+        case TaskStatus.ToDo: return 'info';
+        default: return 'default';
+    }
+};
+
+export const TaskItem = ( {task}: TaskItemProps) => {
+    const navigate = useNavigate();
+
+    return (
+        <Card className={styles.taskCard}>
+            <CardContent>
+                <Typography variant="h6" gutterBottom>
+                    {task.title}
+                </Typography>
+                {task.description && (
+                    <Typography variant="body2" color="text.secondary" paragraph>
+                        {task.description}
+                    </Typography>
+                )}
+                <Box sx={{display:'flex', gap:1, flexWrap:'wrap', mb: 2}}>
+                    <Chip label={task.category} color='primary' size="small" />
+                    <Chip
+                    label={task.status} color={getStatusColor(task.status)} size="small"/>
+
+                </Box>
+                <Button
+                variant="contained"
+                onClick={()=>navigate(`/task/${task.id}`)}
+                >
+                    Edit
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
