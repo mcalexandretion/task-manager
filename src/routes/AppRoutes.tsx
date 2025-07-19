d77740f3393e67839e8e54a8ff422d968e900539
Alexandra <sashaf708@gmail.com>
2025-07-19 20:40:14 +0400

    feat: create AppRoutes

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
+import { TaskList } from '../components/TaskList';
+import { TaskDetails } from '../components/TaskDetails';
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<TaskList />} />
+      <Route path="/task/:id" element={<TaskDetails />} />
+    </Routes>
+  );
+};
